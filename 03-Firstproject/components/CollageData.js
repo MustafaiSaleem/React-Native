01e9190cd3ed76e7a4fadad99b883c004a6f2680
MustafaiSaleem <saleemraza1522@gmail.com>
2023-07-03 12:54:08 +0530

    first commit

---
+++ 03-Firstproject/components/CollageData.js
@@ -0,0 +1,12 @@
+import {Text,View,Button} from 'react-native'
+
+const CollageData=() =>{
+    return(
+        <View>
+        <Text> If You Don't Have Access Then Please Sign Up First </Text>
+        <Button title='Press Here TO Sign Up'></Button>
+        </View>
+    );
+}
+
+export default CollageData;
