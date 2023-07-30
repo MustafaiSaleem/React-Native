5201ba4a529701f531beb1257a04a862855ff318
MustafaiSaleem <saleemraza1522@gmail.com>
2023-07-30 21:28:33 +0530

    First Commit

--- 09-StyleSheet2/App.js
+++ 09-StyleSheet2/App.js
@@ -1,37 +1,44 @@
-import {Button,StyleSheet, Text,View } from 'react-native'
-import React,{useState} from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Toll from './elements/Toll'
 
+export default function App() {
 
-export default function App(){
+  const [name, setname] = useState("Mustafai")
 
-  const [name,setname] =useState("Mustafai")
-  return(
-    <View style={styles.container}>
-      <Text style={{fontSize:30,backgroundColor:'black',padding:4,borderRadius:30,color:'#fff'}}> Hello World</Text>
-      <Button title ='Update props' onPress={()=>setname("Saleem")}/>
-      <Dual name={name}/>
-    </View>
-  );
-}
-
-
-const Dual =(team)=>{
 
   return (
-    <View>
-      <Text style={{fontSize:20}}>{team.name}</Text>
+    <View style={styles.container}>
+      <Text style={[Toll.container]} >Stylesheet</Text>
+
+      <Text style={{ fontSize:15, backgroundColor: 'black', color: 'white', padding: 5, borderRadius: 20, borderWidth: 2, borderColor: 'white' }} > Hello Sir !</Text>
+
+
+      <Button title='Update Props' onPress={() => setname("Saleem")} />
+      <Die name={name} />
     </View>
   );
-
 }
+
+const Die = (team) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 20, color: 'black', backgroundColor: '#fff' }}> {team.name} </Text>
+    </View>
+  );
+}
+
+
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
+    textAlignVertical: 'center',
     justifyContent: 'center',
-    borderRadius:30
+    borderRadius: 30,
+    color: 'black'
   },
 });
--- 10-Temp/App.js
+++ 10-Temp/App.js
@@ -1,25 +1,31 @@
-import {Button,StyleSheet, Text,View } from 'react-native'
-
-
+import {StyleSheet, Text,View} from 'react-native'
+import React,{useState} from 'react'
+import { TextInput } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App(){
 
-  return(
-    <View style={styles.container}>
-      <Text style={{fontSize:30,backgroundColor:'black',padding:4,borderRadius:10,color:'#fff'}}> Hello World </Text>
+  const [name,setname] = useState("")
+
+  return (
+    <View>
+      <Text style={{fontSize:40}}>Handle Text Input ! </Text>
+      <Text style={{fontSize:40}}>Your Name:{name} </Text>
+      <TextInput style={styles.container} placeholder='  Enter Your Name' 
+      onChangeText={(text)=>setname(text)} value={name}
+      />
+      <Button title ='Clear Data' onPress={()=>setname('')}/> 
     </View>
   );
 }
 
 
-
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius:30
+    fontSize:26,
+    textAlignVertical:'bottom',
+    borderWidth:5,
+    borderColor:'black',
+    margin:10
   },
 });
